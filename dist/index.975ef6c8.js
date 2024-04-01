87c8e4f03364ac12700c848178b23cc2592d5782
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-04-01 18:28:36 +0200

    Like functionality

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -586,8 +586,21 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"8lqZg":[function(require,module,exports) {
 const currentUrl = window.location.href;
 const navLinks = document.querySelectorAll("header nav ul li a");
+const likeBtns = document.querySelectorAll(".songs .song span");
 navLinks.forEach((link)=>{
     if (link.href === currentUrl) link.parentElement.classList.add("active");
+});
+likeBtns.forEach((btn)=>{
+    btn.addEventListener("click", (e)=>{
+        let isLiked = e.target.innerText === "favorite_border" ? true : false;
+        if (isLiked) {
+            e.target.textContent = "favorite";
+            isLiked = true;
+        } else {
+            e.target.textContent = "favorite_border";
+            isLiked = false;
+        }
+    });
 });
 
 },{}]},["farZc","8lqZg"], "8lqZg", "parcelRequirea87c")
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const currentUrl = window.location.href;
-
 const navLinks = document.querySelectorAll('header nav ul li a');
+const likeBtns = document.querySelectorAll('.songs .song span');
 
 navLinks.forEach((link) => {
   if (link.href === currentUrl) {
     link.parentElement.classList.add('active');
   }
+});
+
+likeBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    let isLiked = e.target.innerText === 'favorite_border'? true : false;
+
+    if(isLiked){
+      e.target.textContent = 'favorite';
+      isLiked = true;
+    } else {
+      e.target.textContent = 'favorite_border';
+      isLiked = false;
+    }
+  });
 });
