e2aa2dcd88aa5fd2012fc911ecf510fcd42bf9cf
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-05-24 21:16:36 +0200

    animation for the hamburger menu

--- src/menu-functionality.js
+++ src/menu-functionality.js
@@ -1,6 +1,11 @@
 const hamMenu = document.getElementById('ham-menu');
 const navLinks = document.getElementById('nav-links');
+const hamIcon = document.getElementById('ham-icon');
 
-hamMenu.addEventListener('click', function () {
-  navLinks.classList.toggle('clicked');
+hamMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('showMenu');
+});
+
+hamIcon.addEventListener('click', ()=>{
+  hamIcon.classList.toggle('animate')
 });
