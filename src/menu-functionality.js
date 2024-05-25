d42230d47c0c6425f582e0bae5aa21047a3d5436
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-05-25 22:10:15 +0200

    Bug fix menu-functionality for prod mode

--- src/menu-functionality.js
+++ src/menu-functionality.js
@@ -1,11 +1,21 @@
-const hamMenu = document.getElementById('ham-menu');
-const navLinks = document.getElementById('nav-links');
-const hamIcon = document.getElementById('ham-icon');
+document.addEventListener('DOMContentLoaded', function() {
+  const hamMenu = document.getElementById('ham-menu');
+  const navLinks = document.getElementById('nav-links');
+  const hamIcon = document.getElementById('ham-icon');
 
-hamMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('showMenu');
-});
+  if(hamMenu){
+    hamMenu.addEventListener('click', () => {
+      navLinks.classList.toggle('showMenu');
+    });
+  } else {
+    console.log('Element with ID "ham-menu" not found');
+  }
 
-hamIcon.addEventListener('click', () => {
-  hamIcon.classList.toggle('animate')
+  if (hamIcon) {
+      hamIcon.addEventListener('click', () => {
+        hamIcon.classList.toggle('animate');
+      });
+  } else {
+    console.log('Element with ID "ham-icon" not found');
+  }
 });
