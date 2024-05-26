2fcba154a78225211c05c8f99467f28b95d3caa3
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-05-26 11:48:19 +0200

    Bug fixing the animation for ham-menu

--- src/menu-functionality.js
+++ src/menu-functionality.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamMenu = document.getElementById('ham-menu');
   const navLinks = document.getElementById('nav-links');
-  const hamIcon = document.getElementById('ham-icon');
+  const hamIcon = document.querySelector('.hamburger-icon');
 
   if(hamMenu){
     hamMenu.addEventListener('click', () => {
       navLinks.classList.toggle('showMenu');
+      hamIcon.classList.toggle('animate');
     });
   } else {
     console.log('Element with ID "ham-menu" not found');
-  }
-
-  if (hamIcon) {
-      hamIcon.addEventListener('click', () => {
-        hamIcon.classList.toggle('animate');
-      });
-  } else {
-    console.log('Element with ID "ham-icon" not found');
   }
 });
